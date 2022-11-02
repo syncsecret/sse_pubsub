097fc79f8b876a709459c68b2b222bbae9eac7ec
nullrocket <douglas.b.hill@gmail.com>
2022-11-01 23:24:16 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Network address to bind to
+    #[arg(short, long,  default_value = "127.0.0.1")]
+    bind: String,
+
+    /// Network port to use
+    #[arg(short, long, value_parser = clap::value_parser!(u16).range(1..), default_value_t = 8080)]
+    port: u16,
+
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
 }
