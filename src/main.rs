2c409613aa1deb1cf937da3a2807b547190087cb
nullrocket <douglas.b.hill@gmail.com>
2022-11-03 23:22:32 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 
-
 /// SSE pubsub broker
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -12,20 +11,17 @@ struct Args {
     /// Network port to use
     #[arg(short, long, value_parser = clap::value_parser ! (u16).range(1..), default_value_t = 8080)]
     port: u16,
-
 }
-
-
-
 
 use std::net::SocketAddr;
 
 use bytes::{Buf, Bytes};
 use http_body_util::{BodyExt, Full};
-use hyper::server::conn::http1;
+use hyper::server::conn::http2;
 use hyper::service::service_fn;
 use hyper::{body::Incoming as IncomingBody, header, Method, Request, Response, StatusCode};
 use tokio::net::{TcpListener, TcpStream};
+use uuid::Uuid;
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type Result<T> = std::result::Result<T, GenericError>;
@@ -36,6 +32,28 @@ static INTERNAL_SERVER_ERROR: &[u8] = b"Internal Server Error";
 static NOTFOUND: &[u8] = b"Not Found";
 static POST_DATA: &str = r#"{"original": "data"}"#;
 static URL: &str = "http://127.0.0.1:1337/json_api";
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub struct Client {
+    id: Uuid,
+}
+
+impl Client {
+    pub fn new() -> Client {
+        Client { id: Uuid::new_v4() }
+    }
+}
+
+enum Route {
+    NotFound,
+    Publish(Body),
+    Stats,
+    Subscribe,
+}
+#[derive(Clone, Copy)]
+enum Body {
+    Len(u64),
+    Chunked,
+}
 
 async fn subscribe_response() -> Result<Response<BoxBody>> {
     let req = Request::builder()
@@ -80,11 +98,9 @@ async fn publish_response(req: Request<IncomingBody>) -> Result<Response<BoxBody
     Ok(response)
 }
 
-
-
 async fn response_examples(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/")  => Ok(Response::new(full(INDEX))),
+        (&Method::GET, "/") => Ok(Response::new(full(INDEX))),
         (&Method::GET, "/publish") => subscribe_response().await,
         (&Method::POST, "/subscribe") => publish_response(req).await,
         _ => {
@@ -112,19 +128,23 @@ async fn main() -> Result<()> {
         Ok(addr) => {
             let listener = TcpListener::bind(&addr).await?;
             println!("Listening on http://{}", addr);
+            let local = tokio::task::LocalSet::new();
+
             loop {
                 let (stream, _) = listener.accept().await?;
 
-                tokio::task::spawn(async move {
-                    let service = service_fn(response_examples);
-
-                    if let Err(err) = http1::Builder::new()
-                        .serve_connection(stream, service)
-                        .await
-                    {
-                        println!("Failed to serve connection: {:?}", err);
-                    }
-                });
+                local.run_until(async {
+                    local.spawn_local(async {
+                        let service = service_fn(response_examples);
+                        println!("in here");
+                        if let Err(err) = http2::Builder::new(LocalExec)
+                            .serve_connection(stream, service)
+                            .await
+                        {
+                            println!("Failed to serve connection: {:?}", err);
+                        }
+                    }).await.unwrap();
+                }).await;
             }
         }
         Err(err) => {
@@ -132,7 +152,21 @@ async fn main() -> Result<()> {
             Ok(())
         }
     }
+}
+// NOTE: This part is only needed for HTTP/2. HTTP/1 doesn't need an executor.
+//
+// Since the Server needs to spawn some background tasks, we needed
+// to configure an Executor that can spawn !Send futures...
+#[derive(Clone, Copy, Debug)]
+struct LocalExec;
 
-
-
+impl<F> hyper::rt::Executor<F> for LocalExec
+    where
+        F: std::future::Future + 'static, // not requiring `Send`
+{
+    fn execute(&self, fut: F) {
+        println!("in execture");
+        // This will spawn into the currently running `LocalSet`.
+        tokio::task::spawn_local(fut);
+    }
 }
